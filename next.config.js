9ebc282dd8a2d76971967390194e0c3830c641d7
sarmadJamal <sarmadjamal2002@gmail.com>
2025-08-23 04:02:04 +0500

    transpile packages for build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: false,
+  transpilePackages: ["resend", "prettier", "prettier-plugin-tailwindcss"],
   images: {
     domains: ["imgix.cosmicjs.com"], // Add your Cosmic CDN domain
   },
